8f7c9d33162aacb8352096b5985b88ab20d33247
RKatana <roduor41@gmail.com>
2019-10-26 00:46:21 +0300

    update the navbar, change the text color and transition

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,10 +3,15 @@
  (function changeNavColor() {
     $(window).scroll(() => {
       if ($(document).scrollTop() > 100) {
-          $("nav").addClass("navScrollDown").fadeIn()
+          $(".nav-link").addClass("navScrollDown").fadeIn()
+          $('nav').addClass('shadow-lg')
+          $('.fa-bars').addClass('fa-bars-blue')
       }
       else{
-        $("nav").removeClass("navScrollDown")
+         $('.nav-link').removeClass('navScrollDown')
+         $("nav").removeClass("shadow-lg")
+         $('.fa-bars').removeClass('fa-bars-blue')
      }
   })
   })();
+  
