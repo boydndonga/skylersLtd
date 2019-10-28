d3b90ef7b911f48e7519bfd24b3256276d56f827
boydndonga <boydbugle@gmal.com>
2019-10-28 15:34:28 +0300

    create firebase services in auth

--- src/app/admin/auth/auth.service.ts
+++ src/app/admin/auth/auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+  
+// firebase imports
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    public angularFireAuth: AngularFireAuth,
+    public router: Router
+  ) {
+    this.angularFireAuth.authState.subscribe(userResponse => {
+      if (userResponse) {
+        localStorage.setItem('user', JSON.stringify(userResponse));
+      } else {
+        localStorage.setItem('user', null);
+      }
+    }) }
+
+    async login(email: string, password: string) {
+      return await this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
+
+    }
+   
+    async register(email: string, password: string) {
+      return await this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password)
+    }
+   
+    async sendEmailVerification() {
+      return await this.angularFireAuth.auth.currentUser.sendEmailVerification();
+    }
+   
+    async sendPasswordResetEmail(passwordResetEmail: string) {
+      return await this.angularFireAuth.auth.sendPasswordResetEmail(passwordResetEmail);
+    }
+   
+    async logout() {
+      return await this.angularFireAuth.auth.signOut();
+      localStorage.removeItem('user');
+      this.router.navigate(['admin/login']);
+    }
+
+    isUserLoggedIn() {
+      return JSON.parse(localStorage.getItem('user'));
+    }
+   
 }
