52927f1b564a36cf354f3c510d41a3f4fa94c9c1
boydndonga <boydbugle@gmal.com>
2019-10-28 14:06:11 +0300

    create admin routing module with listed route components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BusinessListComponent } from './business-list/business-list.component';
 import { BusinessCreateComponent } from './business-create/business-create.component';
 import { BusinessUpdateComponent } from './business-update/business-update.component';
-import { BusinessDeleteComponent } from './business-delete/business-delete.component';
 import { BusinessComponent } from './business/business.component';
 
 
 
 @NgModule({
-  declarations: [BusinessListComponent, BusinessCreateComponent, BusinessUpdateComponent, BusinessDeleteComponent, BusinessComponent],
+  declarations: [BusinessListComponent, BusinessCreateComponent, BusinessUpdateComponent, BusinessComponent],
   imports: [
     CommonModule
   ]
