5eb829e7a3fb79138f1eb710a274521d0e44a5fa
Newton Karanu <karanunewton4@gmail.com>
2019-10-29 14:56:25 +0300

    fixed merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FooterComponent } from './footer/footer.component';
-import { LandsGalleryComponent} from './lands-gallery/lands-gallery.component';
 import { AdminModule } from './admin/admin.module';
 import {LandsGalleryComponent} from './lands-gallery/lands-gallery.component';
 import { HomeComponent } from './home/home.component';
