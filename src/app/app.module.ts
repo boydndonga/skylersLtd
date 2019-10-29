96c79678dd975cff87c5ecdef3edbb6ee645c972
Newton Karanu <karanunewton4@gmail.com>
2019-10-29 16:00:43 +0300

    fixed merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,20 +14,21 @@ import { from } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 
 // firebase imports
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
- 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {LandsGalleryComponent} from './lands-gallery/lands-gallery.component';
 
-var config = {
-  apiKey: "AIzaSyDkeFvdlaRdQyJ7C00v8tcq1GALX8jBiOE",
-  authDomain: "fir-auth-91d9d.firebaseapp.com",
-  databaseURL: "https://fir-auth-91d9d.firebaseio.com",
-  projectId: "fir-auth-91d9d",
-  storageBucket: "fir-auth-91d9d.appspot.com",
-  messagingSenderId: "286101264214",
-  appId: "1:286101264214:web:7b94e117650106ec8d28f7",
-  measurementId: "G-P41Z3KRWDC"
-}
+
+const config = {
+  apiKey: 'AIzaSyDkeFvdlaRdQyJ7C00v8tcq1GALX8jBiOE',
+  authDomain: 'fir-auth-91d9d.firebaseapp.com',
+  databaseURL: 'https://fir-auth-91d9d.firebaseio.com',
+  projectId: 'fir-auth-91d9d',
+  storageBucket: 'fir-auth-91d9d.appspot.com',
+  messagingSenderId: '286101264214',
+  appId: '1:286101264214:web:7b94e117650106ec8d28f7',
+  measurementId: 'G-P41Z3KRWDC'
+};
 
 @NgModule({
   declarations: [
@@ -43,11 +44,10 @@ var config = {
     BrowserModule,
     AppRoutingModule,
     AdminModule,
-    HttpClientModule
+    HttpClientModule,
     AngularFireModule.initializeApp(config),
     AngularFireAuthModule,
     FormsModule,
-    AngularSvgIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
