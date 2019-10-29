a28d46a93f6ce0dfdda580fe56017a44aeee1f64
RKatana <roduor41@gmail.com>
2019-10-29 12:52:06 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { LandsGalleryComponent } from './lands-gallery/lands-gallery.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutUsComponent
+  },
+  {
+    path:'gallery',
+    component: LandsGalleryComponent
   },
 ];
 
