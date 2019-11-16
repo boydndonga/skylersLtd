385560af6305803746858f351ed936ba29bf7cb3
RKatana <roduor41@gmail.com>
2019-11-16 19:07:03 +0300

    add busines dev component

---
+++ src/app/b-dev/b-dev.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-b-dev',
+  templateUrl: './b-dev.component.html',
+  styleUrls: ['./b-dev.component.css']
+})
+export class BDevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
