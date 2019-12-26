28f84f34dd16cb6bb308a75ef90d217113be7aea
boydndonga <boydbugle@gmal.com>
2019-12-27 00:42:16 +0300

    update navbar component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDkeFvdlaRdQyJ7C00v8tcq1GALX8jBiOE',
+    authDomain: 'fir-auth-91d9d.firebaseapp.com',
+    databaseURL: 'https://fir-auth-91d9d.firebaseio.com',
+    projectId: 'fir-auth-91d9d',
+    storageBucket: 'fir-auth-91d9d.appspot.com',
+    messagingSenderId: '286101264214',
+    appId: '1:286101264214:web:7b94e117650106ec8d28f7',
+    measurementId: 'G-P41Z3KRWDC'
+  }
 };
